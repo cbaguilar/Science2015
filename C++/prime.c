a475c581c0d161188c4aa52e1f2dc1db0a8ae368
Mr. Frog <christian20002@users.noreply.github.com>
2015-11-23 09:21:26 -0800

    Cleaned up code, added comments

--- C++/prime.c
+++ C++/prime.c
@@ -4,49 +4,48 @@
 #include <ctime>
 using namespace std;
 
+//program for calculating to the nth prime number
+
 int main(){
 	
-	
-	int a;
 	struct timeval stop, start;
 	gettimeofday(&start,NULL);
-	//cin >> a;
-//	cout << elapsed << endl;
-	
+
 	double curnum = 2.0;
 	double curdiv = 2.0;
-	
+	/*
+	My method of determining whether or not a number is prime involves dividing every number below it as
+	an Int and as a Double. If the rounded answer is equal to the unrounded answer, then that means it can be divided.
+	*/
 	int rnd;
 	double dbl;
 
-	for (int i = 0; i < 1000;){
+	for (int i = 0; i < 1000;){ 	//specify exactly how many primes to find.
 		
 		curdiv  = 2;
 		while (curdiv!= curnum){
-			//cout << "bope" << endl;
+			
 			rnd = curnum/curdiv;
-			//cout << rnd <<endl;
 			dbl =  curnum/curdiv;
-			//out << dbl << endl;
-			if (rnd == dbl){
 
+			if (rnd == dbl){
 				break;
 			}
+			
 			curdiv++;
-		//cout << curnum << endl;
 		}
-
-	if (curnum == curdiv){
-		cout << curnum << endl;
-		i++;
-		}
-	curnum++;
 		
+		if (curnum == curdiv){
+			cout << curnum << endl;
+			i++;
+		}
+		
+		curnum++;
 	}
+	
 	gettimeofday(&stop,NULL);
 	long elapsed = stop.tv_usec - start.tv_usec;
-cout << elapsed << endl;
-double dl = 1/3;
-//cout << dl << endl;
-return 0;
+	cout << elapsed << endl;
+	double dl = 1/3;
+	return 0;
 }
